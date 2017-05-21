52219ec62f77222d519f92b6d18b91e23cd87114
Algenis E. Volquez <evolquez@gmail.com>
2017-05-21 12:22:15 -0400

    Preparing layout structure

--- app/src/main/java/com/oletob/calculadorarpn/ui/MainActivity.java
+++ app/src/main/java/com/oletob/calculadorarpn/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.oletob.calculadorarpn;
+package com.oletob.calculadorarpn.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.oletob.calculadorarpn.R;
 
 public class MainActivity extends AppCompatActivity {
 
