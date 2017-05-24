5b9ebd91d2fbeb646a2e60ad3c1faaa26514cec5
Algenis E. Volquez <evolquez@gmail.com>
2017-05-24 10:34:25 -0400

    Added app icons

--- app/src/main/java/com/oletob/rpncalc/ui/MainActivity.java
+++ app/src/main/java/com/oletob/rpncalc/ui/MainActivity.java
@@ -3,6 +3,7 @@ package com.oletob.rpncalc.ui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
