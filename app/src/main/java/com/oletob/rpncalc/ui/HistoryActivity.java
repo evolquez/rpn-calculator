a3ccd91770122ca726bf7f9661d51f4be20274f4
Algenis E. Volquez <evolquez@gmail.com>
2017-05-27 00:15:17 -0400

    Clear history; Added menu that contains the clear selection, create method loadHistoy on HistoryActivity for clear the listView when history cleared

--- app/src/main/java/com/oletob/rpncalc/ui/HistoryActivity.java
+++ app/src/main/java/com/oletob/rpncalc/ui/HistoryActivity.java
@@ -1,9 +1,13 @@
 package com.oletob.rpncalc.ui;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -15,7 +19,7 @@ import com.oletob.rpncalc.model.Rpn;
 import java.util.ArrayList;
 
 public class HistoryActivity extends AppCompatActivity {
-
+    private SharedPreferences history;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,7 +28,36 @@ public class HistoryActivity extends AppCompatActivity {
         // Set action bar title to "History"
         getSupportActionBar().setTitle("History");
 
-        SharedPreferences history = getSharedPreferences(Rpn.KEY, Context.MODE_PRIVATE);
+        this.loadHistory();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.clear_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.btnClear:
+                SharedPreferences.Editor editor = this.history.edit();
+                editor.clear();
+
+                if(editor.commit()){
+                    this.loadHistory();
+                    Toast.makeText(this, "History cleared!", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void loadHistory(){
+        history = getSharedPreferences(Rpn.KEY, Context.MODE_PRIVATE);
 
         ArrayList<HistoryHolder> historyItems =  Rpn.loadHistoryInArray(history.getString(Rpn.KEY, "NONE"));
 
@@ -32,6 +65,5 @@ public class HistoryActivity extends AppCompatActivity {
 
         // Set the adapter to the listView
         ((ListView) findViewById(R.id.listHistory)).setAdapter(adapter);
-
     }
 }
