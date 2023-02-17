a09d12c9babd124ed70007333f1861b0993a367c
Algenis <evolquez@gmail.com>
2023-02-17 17:44:01 -0400

    Initial commit for refactoring

--- app/src/main/java/com/oletob/rpncalc/ui/main/MainActivity.kt
+++ app/src/main/java/com/oletob/rpncalc/ui/main/MainActivity.kt
@@ -3,55 +3,55 @@ package com.oletob.rpncalc.ui.main
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Button
-import android.widget.TextView
+import androidx.activity.viewModels
 import com.oletob.rpncalc.R
+import com.oletob.rpncalc.databinding.ActivityMainBinding
 import com.oletob.rpncalc.ui.about.AboutActivity
 import com.oletob.rpncalc.ui.common.BaseActivity
 import com.oletob.rpncalc.ui.history.HistoryActivity
 
 class MainActivity: BaseActivity(), MainContract.View {
 
-    private lateinit var panelTextView: TextView
     private lateinit var presenter: MainPresenter
 
-    //Numbered buttons
-    private val numberButtons = listOf(
-        R.id.button_0,
-        R.id.button_1,
-        R.id.button_2,
-        R.id.button_3,
-        R.id.button_4,
-        R.id.button_5,
-        R.id.button_6,
-        R.id.button_7,
-        R.id.button_8,
-        R.id.button_9
-    )
+    private val mainViewModel: MainViewModel by viewModels()
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
 
         presenter = MainPresenter(this)
 
-        panelTextView = findViewById(R.id.text_view_panel)
+        initClickListeners()
+    }
 
-        // Buttons control
-        findViewById<Button>(R.id.button_clear).setOnClickListener{presenter.onClickClear()}
-        findViewById<Button>(R.id.button_enter).setOnClickListener{presenter.onClickEnter()}
-        findViewById<Button>(R.id.button_delete).setOnClickListener{presenter.onClickDelete()}
+    private fun initClickListeners() {
 
-        numberButtons.forEach{
-            val button = findViewById<Button>(it)
-            button.setOnClickListener{presenter.onClickNumber(button.text.toString().toInt())}
+        //Numbered buttons
+        val numberButtons = listOf(binding.button0, binding.button1, binding.button2, binding.button3, binding.button4, binding.button5, binding.button6, binding.button7, binding.button8, binding.button9)
+
+        with(binding){
+
+            // Buttons control
+            buttonClear.setOnClickListener{presenter.onClickClear()}
+            buttonEnter.setOnClickListener{presenter.onClickEnter()}
+            buttonDelete.setOnClickListener{presenter.onClickDelete()}
+
+            numberButtons.forEach {button ->
+                button.setOnClickListener{presenter.onClickNumber(button.text.toString().toInt())}
+            }
+
+            // Operators
+            buttonDivide.setOnClickListener{presenter.onClickOperator(MainPresenter.Operator.DIVIDE)}
+            buttonMultiply.setOnClickListener{presenter.onClickOperator(MainPresenter.Operator.MULTIPLY)}
+            buttonSubtract.setOnClickListener{presenter.onClickOperator(MainPresenter.Operator.SUBTRACT)}
+            buttonSum.setOnClickListener{presenter.onClickOperator(MainPresenter.Operator.SUM)}
         }
-
-        // Operators
-        findViewById<Button>(R.id.button_divide).setOnClickListener{presenter.onClickOperator(MainPresenter.Operator.DIVIDE)}
-        findViewById<Button>(R.id.button_multiply).setOnClickListener{presenter.onClickOperator(MainPresenter.Operator.MULTIPLY)}
-        findViewById<Button>(R.id.button_subtract).setOnClickListener{presenter.onClickOperator(MainPresenter.Operator.SUBTRACT)}
-        findViewById<Button>(R.id.button_sum).setOnClickListener{presenter.onClickOperator(MainPresenter.Operator.SUM)}
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -68,13 +68,13 @@ class MainActivity: BaseActivity(), MainContract.View {
         return true
     }
 
-    override fun getPanelText() = panelTextView.text.toString()
+    override fun getPanelText() = binding.textViewPanel.text.toString()
 
     override fun setPanelText(text: String) {
-        panelTextView.text = text
+        binding.textViewPanel.text = text
     }
 
     override fun appendToPanelText(text: String) {
-        panelTextView.append(text)
+        binding.textViewPanel.append(text)
     }
 }
---
+++ app/src/main/java/com/oletob/rpncalc/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.oletob.rpncalc.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+
+}
